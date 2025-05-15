3860643d0d0bbe5dfcde0a913074f5c1bbbf0d99
hombresenforma <ivan.hombresenforma@gmail.com>
2025-05-15 18:03:43 +0200

    ivan-rutina2.js

--- ivan-rutina2.js
+++ ivan-rutina2.js
@@ -151,40 +151,57 @@ const workoutData = {
 };
 
 const exerciseAlternatives = {
-    "Press de Banca con Barra": [{ name: "Press de Banca con Mancuernas" }, { name: "Flexiones con Lastre"}],
-    "Press Banca con Barra": [{ name: "Press de Banca con Mancuernas" }, { name: "Flexiones con Lastre"}], // Duplicado para cubrir ambas formas de nombrarlo
-    "Press Inclinado con Mancuernas": [{ name: "Press Inclinado con Barra" }, { name: "Flexiones Inclinadas (manos elevadas)" }],
-    "Fondos en Paralelas": [{ name: "Press Declinado con Barra" }, { name: "Fondos entre Bancos (Tríceps)" }],
-    "Extensiones Tríceps Polea Alta con Cuerda": [{ name: "Press Francés con Barra Z" }, { name: "Extensiones sobre Cabeza con Mancuerna" }],
-    "Triceps Trasnuca con Mancuernas/Kettlebell": [{ name: "Press Francés con Barra Z" }, { name: "Extensiones Tríceps Polea Alta con Cuerda" }],
-    "Elevaciones Laterales con Mancuerna": [{ name: "Elevaciones Laterales en Polea" }, { name: "Upright Row con Barra (agarre ancho)" }],
-    "Remo en Barra T con Apoyo en Pecho": [{ name: "Remo con Mancuerna a un Brazo (con apoyo)" }, { name: "Remo en Máquina con Apoyo al Pecho" }],
-    "Curl de Bíceps con Barra Z": [{ name: "Curl de Bíceps con Barra Recta"}, { name: "Curl de Bíceps con Mancuernas"}],
-    "Curl con Barra Z": [{ name: "Curl de Bíceps con Barra Recta"}, { name: "Curl de Bíceps con Mancuernas (simultáneo o alterno)"}], // Actualizado para coincidir con el formato
-    "Jalón al Pecho en Polea Alta (Agarre Ancho Prono)": [{ name: "Dominadas (asistidas o libres)"}, {name: "Remo con Barra"}],
-    "Jalón al Pecho en Polea": [{ name: "Dominadas (asistidas o libres)"}, {name: "Jalón al Pecho con Agarre Estrecho"}],
-    "Curl Concentrado con Mancuerna (en banco Scott si es posible)": [{ name: "Curl Concentrado con Mancuerna (sentado)"}, {name: "Curl Araña con Mancuernas"}],
-    "Curl Femoral Sentado en Máquina": [{ name: "Curl Femoral Tumbado"}, {name: "Peso Muerto Rumano con Mancuernas"}],
-    "Curl Femoral en Máquina": [{ name: "Curl Femoral Tumbado en Máquina"}, {name: "Nordic Curl"}],
-    "Remo Gorila con Mancuernas o KTB": [{ name: "Remo con Mancuerna a un Brazo (sin apoyo)"}, {name: "Renegade Row (Remo Renegado)"}],
-    "Face Pull con Cuerda en Polea": [{ name: "Band Pull-Aparts" }, { name: "Pájaros con Mancuernas (Peck Deck Inverso)" }],
-    "Sentadilla Trasera con Barra": [{ name: "Prensa Inclinada" }, { name: "Sentadilla Goblet" }],
-    "Hack Squat": [{ name: "Prensa Inclinada" }, { name: "Sentadilla Frontal con Barra" }],
-    "Remo con Barra (Agarre Prono)": [{ name: "Remo con Mancuernas (ambos brazos)" }, { name: "Remo en Punta con Barra T" }],
-    "Remo con Barra": [{ name: "Remo con Mancuernas (ambos brazos)" }, { name: "Remo Pendlay" }],
-    "Encogimientos Abdominales (Crunches)": [{ name: "Encogimientos en Polea Alta" }, { name: "Rueda Abdominal (Ab Wheel Rollout)" }],
-    "Plancha Abdominal (Plank)": [{ name: "Plancha Lateral" }, { name: "Plancha con Elevación de Brazo/Pierna" }],
-    "Elevaciones de Piernas Tumbado": [{ name: "Elevaciones de Piernas Colgado (Hanging Leg Raises)" }, { name: "Encogimientos Inversos (Reverse Crunches)" }],
-    "Peso Muerto Rumano con Mancuernas": [{ name: "Peso Muerto Rumano con Barra" }, { name: "Good Mornings con Barra" }],
-    "Peso Muerto Rumano con Barra / Deadlift Romanian Barbell": [{ name: "Peso Muerto Rumano con Mancuernas" }, { name: "Good Mornings con Barra" }],
-    "Prensa Inclinada": [{ name: "Sentadilla Hack" }, { name: "Zancadas con Barra o Mancuernas" }],
-    "Cruces en Polea Alta": [{ name: "Aperturas con Mancuernas en Banco Plano" }, { name: "Aperturas en Máquina Contractora (Pec Deck)" }],
-    "Extensión de rodilla en Máquina": [{ name: "Sentadilla Sissy (con o sin peso)" }, { name: "Zancadas Cortas" }],
-    "Pull Over en Polea alta / Cross over Pull Down": [{ name: "Pullover con Mancuerna" }, { name: "Pull Over en Máquina" }],
-    "Elevación de Talones de Pie (en máquina o con peso)": [{ name: "Elevación de Talones Sentado (para sóleo)" }, { name: "Elevación de Talones en Prensa" }]
+    "Press Banca con Barra": [
+        { name: "Press Banca con Mancuernas", videoUrl: "https://youtu.be/hXCJC2ApcdgPress" },
+        { name: "Flexiones / Push Ups", videoUrl: "https://www.youtube.com/watch?v=GFUw7S0wDxkFlexiones" }
+    ],
+    "Hack Squat": [
+        { name: "Prensa Inclinada 45", videoUrl: "https://www.youtube.com/watch?v=IqjbBRNqJpsPrensa" },
+        { name: "Sentadilla Frontal con Barra", videoUrl: "https://youtu.be/ErXO1WKVlPISentadilla" }
+    ],
+    "Press Inclinado con Mancuernas": [
+        { name: "Press Banca Inclinado", videoUrl: "https://www.youtube.com/watch?v=4tPP-4K5kMQPress" },
+        { name: "Press en Máquina inclinado", videoUrl: "https://www.youtube.com/watch?v=nna4gQD-v9YAperturas" }
+    ],
+    "Cruces en Polea Alta": [
+        { name: "Aperturas en Banco con Mancuernas", videoUrl: "https://www.youtube.com/watch?v=dfmq1UOuUXoAperturas" },
+        { name: "Aperturas en Máquina Contractora / Mariposa", videoUrl: "https://www.youtube.com/watch?v=gMzwVArofx0Aperturas" }
+    ],
+    "Extensión de rodilla en Máquina": [
+        { name: "Zancadas Caminando con Mancuernas/Kettlebells - Cuádriceps", videoUrl: "https://youtu.be/7tRy9X0ibnkZancadas" },
+        { name: "Sentadilla Goblet con Mancuerna/Kettlebell", videoUrl: "https://youtu.be/xPVfWX_9ytkSentadilla" }
+    ],
+    "Triceps Trasnuca con Mancuernas/Kettlebell": [
+        { name: "Press Francés con Barra Z", videoUrl: "https://www.youtube.com/watch?v=k8KH84637RwPress" },
+        { name: "Extensión de Codo en Polea Alta", videoUrl: "https://www.youtube.com/watch?v=z7gqbcjkzU0Extensión" }
+    ],
+    "Remo con Barra": [
+        { name: "Remo con Mancuerna Unilateral", videoUrl: "https://youtu.be/EPXL9QIasLMRemo" },
+        { name: "Remo en Barra T", videoUrl: "https://www.youtube.com/watch?v=AgxCPp37waQRemo" }
+    ],
+    "Peso Muerto Rumano con Barra / Deadlift Romanian Barbell": [
+        { name: "Peso Muerto Rumano con Mancuernas / Deadlift Romanian DBs", videoUrl: "https://www.youtube.com/watch?v=mpg_qmBdmxcPeso" },
+        { name: "Buenos Días con Barra", videoUrl: "https://youtu.be/IKNPwb8BSmMThruster" }
+    ],
+    "Jalón al Pecho en Polea": [
+        { name: "Dominadas / Pull Ups", videoUrl: "https://www.youtube.com/watch?v=7ht8tmdAyqwDominadas" },
+        { name: "Jalón al Pecho Supino en Polea", videoUrl: "https://youtu.be/rimdRzyIJkAJalón" }
+    ],
+    "Pull Over en Polea alta / Cross over Pull Down": [
+        { name: "Pullover con Kettlebell", videoUrl: "https://www.youtube.com/watch?v=aGiAvWpeyAIESPALDADominadas" },
+        { name: "Face Pull en Polea Alta", videoUrl: "https://www.youtube.com/watch?v=tZN8KYDSCRoFace" }
+    ],
+    "Curl Femoral en Máquina": [
+        { name: "Nordic Curl", videoUrl: "https://www.youtube.com/watch?v=N_jZzObyGxECurl" },
+        { name: "Curl Femoral desde Puente (Con Sliders / Toalla)", videoUrl: "https://www.youtube.com/watch?v=HI3Bb1SAru8Adductores" }
+    ],
+    "Curl con Barra Z": [
+        { name: "Curl con Mancuernas de Pie", videoUrl: "https://youtu.be/8STuQuoDMR0Curl" },
+        { name: "Curl Martillo con Mancuernas", videoUrl: "https://youtu.be/fcFsPoJY9lgCurl" }
+    ]
 };
 // =================================================================================
 // FIN DE DATOS DE LA RUTINA Y ALTERNATIVAS
 // =================================================================================
-console.log("Datos de rutina para Ivan cargados desde ivan-rutina2.js");
+console.log("Datos de rutina para Ivan cargados desde ivan-rutina.js. Alternativas actualizadas con videos.");
 
