e462a42798c98fcdec89550615a88cfd68b6d439
hombresenforma <ivan.hombresenforma@gmail.com>
2025-06-01 14:28:29 +0200

    elena_prueba.js

--- elena_prueba.js
+++ elena_prueba.js
@@ -1,11 +1,11 @@
 // Contenido de app_datos.js
 
 // =================================================================================
-// DATOS DE LA RUTINA (1 DÍAS)
+// DATOS DE LA RUTINA (3 DÍAS)
 // =================================================================================
 const workoutData = {
     "dia1": {
-        name: "DÍA 1: Pierna Completa",
+        name: "Pierna Completa",
         exercises: [
             {
                 order: 1,
@@ -13,8 +13,7 @@ const workoutData = {
                 sets: 4,
                 reps: "6, 6, 10, 10",
                 rest: "120s",
-                rpe: "N/A",
-                notes: "Hazlo de espaldas",
+                notes: "",
                 videoUrl: "https://www.youtube.com/watch?v=6teL-OyXuQsThruster"
             },
             {
@@ -22,37 +21,189 @@ const workoutData = {
                 name: "Prensa Inclinada 45",
                 sets: 3,
                 reps: "12, 10, 8",
-                rest: "90s",
-                rpe: "N/A",
-                notes: "Subiendo peso en cada serie",
+                rest: "120s",
+                notes: "",
                 videoUrl: "https://www.youtube.com/watch?v=IqjbBRNqJpsPrensa"
             },
             {
                 order: 3,
+                name: "Curl Femoral en Máquina",
                 isSuperset: true,
                 items: [
                     {
+                        name: "Curl Femoral en Máquina",
                         subOrder: 1,
-                        name: "Sentadilla Goblet con Mancuerna/Kettlebell",
                         sets: 3,
                         reps: "12",
                         isSupersetStart: true,
-                        customRestText: "90",
-                        rpe: "N/A",
+                        notes: "undefined",
+                        videoUrl: "https://www.youtube.com/watch?v=Zhnhz1r_u-YCurl"
+
+                    },
+                    {
+                        name: "Zancadas Caminando con Mancuernas/Kettlebells - Cuádriceps",
+                        subOrder: 2,
+                        sets: 3,
+                        reps: "16",
                         notes: "",
-                        videoUrl: "https://youtu.be/xPVfWX_9ytkSentadilla"
+                        videoUrl: "https://youtu.be/7tRy9X0ibnkZancadas"
+                        ,rest: "90s"
                     }
-                ]
+                ],
+                notes: "",
+                videoUrl: "https://www.youtube.com/watch?v=Zhnhz1r_u-YCurl"
             },
             {
                 order: 4,
+                name: "Aductor en Maquina",
+                sets: 3,
+                reps: "12",
+                rest: "60s",
+                notes: "",
+                videoUrl: "https://www.youtube.com/watch?v=CfTZfTILu3UAdd"
+            }
+        ]
+    },
+    "dia2": {
+        name: "Torso Completo",
+        exercises: [
+            {
+                order: 1,
+                name: "Remo en Barra T",
+                sets: 3,
+                reps: "8, 8, 12",
+                rest: "90s",
+                notes: "",
+                videoUrl: "https://www.youtube.com/watch?v=AgxCPp37waQRemo"
+            },
+            {
+                order: 2,
+                name: "Press Militar Sentado con Mancuernas",
+                sets: 3,
+                reps: "8, 8, 12",
+                rest: "90s",
+                notes: "",
+                videoUrl: "https://www.youtube.com/watch?v=_IMpMCr87Cg&list=PLDmmv3PadX2NuaxMhWe4DMVBnkUuHymCK&index=187Press"
+            },
+            {
+                order: 3,
+                name: "Jalón al Pecho en Polea",
+                isSuperset: true,
+                items: [
+                    {
+                        name: "Jalón al Pecho en Polea",
+                        subOrder: 1,
+                        sets: 3,
+                        reps: "10",
+                        isSupersetStart: true,
+                        notes: "undefined",
+                        videoUrl: "https://youtu.be/GYIhmy1P4vYJalón"
+
+                    },
+                    {
+                        name: "Elevaciones Laterales con Mancuernas",
+                        subOrder: 2,
+                        sets: 3,
+                        reps: "12",
+                        notes: "",
+                        videoUrl: "https://youtu.be/rhmW_fhB4csElevaciones"
+                        ,rest: "90s"
+                    }
+                ],
+                notes: "",
+                videoUrl: "https://youtu.be/GYIhmy1P4vYJalón"
+            },
+            {
+                order: 4,
+                name: "EMOM 8 min",
+                isSuperset: true,
+                isEMOM: true,
+                emomDetails: {
+                    totalIntervals: 8,
+                    workIntervalSeconds: 60
+                },
+                items: [
+                    {
+                        subOrder: 1,
+                        name: "Fondos en Banco",
+                        sets: 1,
+                        reps: "12",
+                        isEMOMItem: true,
+                        videoUrl: "https://youtu.be/NSB_HrDwd6IFondos"
+                    },
+                    {
+                        subOrder: 2,
+                        name: "Curl Martillo con Mancuernas",
+                        sets: 1,
+                        reps: "10",
+                        isEMOMItem: true,
+                        videoUrl: "https://youtu.be/fcFsPoJY9lgCurl"
+                    }
+                ],
+                notes: "",
+                videoUrl: "https://youtu.be/NSB_HrDwd6IFondos"
+            }
+        ]
+    },
+    "dia3": {
+        name: "Pierna Foco Glúteo",
+        exercises: [
+            {
+                order: 1,
+                name: "Hip Thrust con Barra",
+                sets: 3,
+                reps: "8, 8, 12",
+                rest: "120s",
+                notes: "",
+                videoUrl: "https://youtu.be/sbl1qYdkzUcHip"
+            },
+            {
+                order: 2,
+                name: "Sentadilla Búlgara con Mancuerna",
+                sets: 3,
+                reps: "8",
+                rest: "90s",
+                notes: "Puedes hacerlas en multipower",
+                videoUrl: "https://youtu.be/kA6bHiDdTO4Sentadilla"
+            },
+            {
+                order: 3,
                 name: "Step Up Unilateral en Step con Mancuerna/Kettlebell",
                 sets: 3,
                 reps: "10",
                 rest: "90s",
-                rpe: "N/A",
-                notes: "Subiendo peso en cada serie",
+                notes: "Puedes hacerlas en multipower",
                 videoUrl: "https://youtu.be/cabSAbO0YLYSplit"
+            },
+            {
+                order: 4,
+                name: "EMOM 8 min",
+                isSuperset: true,
+                isEMOM: true,
+                emomDetails: {
+                    totalIntervals: 8,
+                    workIntervalSeconds: 60
+                },
+                items: [
+                    {
+                        subOrder: 1,
+                        name: "Zancadas Caminando con Mancuernas/Kettlebells",
+                        sets: 1,
+                        reps: "12",
+                        isEMOMItem: true,
+                        videoUrl: "https://youtu.be/xyl28TxLlsMMZancadas"
+                    },
+                    {
+                        subOrder: 2,
+                        name: "Swing Ruso con Kettlebell",
+                        sets: 1,
+                        reps: "12",
+                        isEMOMItem: true,
+                        videoUrl: "https://youtu.be/eKN0tj8q6QcSwing"
+                    }
+                ],
+                notes: "",
+                videoUrl: "https://youtu.be/xyl28TxLlsMMZancadas"
             }
         ]
     }
@@ -60,7 +211,10 @@ const workoutData = {
 
 const exerciseAlternatives = {
     "Hack Squat": [
-        { name: "Sentadilla Trasera con Barra", videoUrl: "https://youtu.be/FK5XU_gaxAESentadilla" }
+        { name: "Sentadilla Trasera en Multipower", videoUrl: "https://youtu.be/la-dqygoIukSentadilla" }
+    ],
+    "Remo en Barra T": [
+        { name: "Remo con Mancuerna Unilateral", videoUrl: "https://youtu.be/EPXL9QIasLMRemo" }
     ]
 };
 // =================================================================================
