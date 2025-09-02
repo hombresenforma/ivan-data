dc7b88fd8cfd8878bfaeceb4cb27571d1524ea7c
hombresenforma <ivan.hombresenforma@gmail.com>
2025-09-02 10:55:23 +0200

    Update luisu.js

--- luisu.js
+++ luisu.js
@@ -1,7 +1,7 @@
 // Contenido de app_datos.js
 
 // =================================================================================
-// DATOS DE LA RUTINA (4 DÍAS)
+// DATOS DE LA RUTINA (5 DÍAS)
 // =================================================================================
 const workoutData = {
     "dia1": {
@@ -327,8 +327,8 @@ const workoutData = {
                         sets: 1,
                         reps: "45s",
                         isCircuitItem: true,
-                        videoUrl: "https://www.youtube.com/shorts/KKvRtNn904g",
-                        imageUrl: "https://i.ytimg.com/vi/KKvRtNn904g/mqdefault.jpg"
+                        videoUrl: "https://www.youtube.com/shorts/KKvRtN904g",
+                        imageUrl: ""
                     },
                     {
                         name: "Cardio - Correr en Cinta Suave",
@@ -336,13 +336,13 @@ const workoutData = {
                         sets: 1,
                         reps: "45s",
                         isCircuitItem: true,
-                        videoUrl: "https://www.youtube.com/shorts/KKvRtNn904g",
-                        imageUrl: "https://i.ytimg.com/vi/KKvRtNn904g/mqdefault.jpg"
+                        videoUrl: "https://www.youtube.com/shorts/KKvRtN904g",
+                        imageUrl: ""
                     }
                 ],
                 notes: "SPRINT: Por encima de 11km/h\nSUAVE: Por encima de 7km/h",
-                videoUrl: "https://www.youtube.com/shorts/KKvRtNn904g",
-                imageUrl: "https://i.ytimg.com/vi/KKvRtNn904g/mqdefault.jpg"
+                videoUrl: "https://www.youtube.com/shorts/KKvRtN904g",
+                imageUrl: ""
             },
             {
                 order: 2,
@@ -403,6 +403,113 @@ const workoutData = {
                 notes: "",
                 videoUrl: "https://youtu.be/XJFii9NYHNs",
                 imageUrl: "https://i.ytimg.com/vi/XJFii9NYHNs/mqdefault.jpg"
+            }
+        ]
+    },
+    "dia5": {
+        name: "Descarga (Foam Roller)",
+        exercises: [
+            {
+                order: 1,
+                name: "Movilidad Articular",
+                isSuperset: true,
+                circuitDetails: {
+                    totalRounds: 2,
+                    restBetweenExercisesSeconds: 5,
+                    restBetweenRoundsSeconds: 3
+                },
+                items: [
+                    {
+                        name: "Movilidad - Columna",
+                        subOrder: 1,
+                        sets: 1,
+                        reps: "30s",
+                        isCircuitItem: true,
+                        videoUrl: "https://youtu.be/48_aYaSLNRY",
+                        imageUrl: "https://i.ytimg.com/vi/48_aYaSLNRY/mqdefault.jpg"
+                    },
+                    {
+                        name: "Movilidad - Activación de Escápulas",
+                        subOrder: 2,
+                        sets: 1,
+                        reps: "30s",
+                        isCircuitItem: true,
+                        videoUrl: "https://youtu.be/6Kj_rg_7tYQ",
+                        imageUrl: "https://i.ytimg.com/vi/6Kj_rg_7tYQ/mqdefault.jpg"
+                    },
+                    {
+                        name: "Movilidad - De Cadera",
+                        subOrder: 3,
+                        sets: 1,
+                        reps: "30s",
+                        isCircuitItem: true,
+                        videoUrl: "https://youtu.be/7TmNRUP7N_0",
+                        imageUrl: "https://i.ytimg.com/vi/7TmNRUP7N_0/mqdefault.jpg"
+                    }
+                ],
+                notes: "Exagerar las curvaturas.",
+                videoUrl: "https://youtu.be/48_aYaSLNRY",
+                imageUrl: "https://i.ytimg.com/vi/48_aYaSLNRY/mqdefault.jpg"
+            },
+            {
+                order: 2,
+                name: "Liberación Miofascial - Gastrocnemios",
+                isSuperset: true,
+                items: [
+                    {
+                        name: "Liberación Miofascial - Gastrocnemios",
+                        subOrder: 1,
+                        sets: 1,
+                        reps: "10",
+                        isSupersetStart: true,
+                        videoUrl: "https://youtu.be/juUqLPfhF2I",
+                        imageUrl: "https://i.ytimg.com/vi/juUqLPfhF2I/mqdefault.jpg"
+                    },
+                    {
+                        name: "Liberación Miofascial - Isquiosurales",
+                        subOrder: 2,
+                        sets: 1,
+                        reps: "10",
+                        videoUrl: "https://youtu.be/wfP97EW_3B0",
+                        imageUrl: "https://i.ytimg.com/vi/wfP97EW_3B0/mqdefault.jpg"
+                    },
+                    {
+                        name: "Liberación Miofascial - Glúteo y Piramidal",
+                        subOrder: 3,
+                        sets: 1,
+                        reps: "10",
+                        videoUrl: "https://youtu.be/H4TxDBt9Oew",
+                        imageUrl: "https://i.ytimg.com/vi/H4TxDBt9Oew/mqdefault.jpg"
+                    },
+                    {
+                        name: "Liberación Miofascial - Cuádriceps Distal",
+                        subOrder: 4,
+                        sets: 1,
+                        reps: "10",
+                        videoUrl: "https://youtu.be/q27nZmlg_x8",
+                        imageUrl: "https://i.ytimg.com/vi/q27nZmlg_x8/mqdefault.jpg"
+                    },
+                    {
+                        name: "Liberación Miofascial - Iliopsoas y Cuádriceps",
+                        subOrder: 5,
+                        sets: 1,
+                        reps: "10",
+                        videoUrl: "https://youtu.be/4Ic63y0cRgQ",
+                        imageUrl: "https://i.ytimg.com/vi/4Ic63y0cRgQ/mqdefault.jpg"
+                    },
+                    {
+                        name: "Liberación Miofascial - TFL",
+                        subOrder: 6,
+                        sets: 1,
+                        reps: "10",
+                        rest: "60s",
+                        videoUrl: "https://youtu.be/6bO-z38wmdk",
+                        imageUrl: "https://i.ytimg.com/vi/6bO-z38wmdk/mqdefault.jpg"
+                    }
+                ],
+                notes: "Liberación Miofascial. Busca un punto de dolor (punto gatillo) e insiste en pasadas por esa zona. Entre 10 y 20 pasadas por zona. ",
+                videoUrl: "https://youtu.be/juUqLPfhF2I",
+                imageUrl: "https://i.ytimg.com/vi/juUqLPfhF2I/mqdefault.jpg"
             }
         ]
     }
