f52c05752d77d6b7139a4ba7ed1cf4e5838635ae
hombresenforma <ivan.hombresenforma@gmail.com>
2025-09-16 06:52:47 +0200

    Update pruebas.js

--- pruebas.js
+++ pruebas.js
@@ -79,6 +79,91 @@ const workoutData = {
                 notes: "",
                 videoUrl: "https://www.youtube.com/shorts/00FzH7YaDwc",
                 imageUrl: "https://i.ytimg.com/vi/00FzH7YaDwc/mqdefault.jpg"
+            },
+            {
+                order: 3,
+                name: "Press Banca Declinado con Barra",
+                sets: 3,
+                reps: "10-12",
+                rest: "60s",
+                notes: "",
+                videoUrl: "https://www.youtube.com/watch?v=UWD-mpupcBw",
+                imageUrl: "https://i.ytimg.com/vi/UWD-mpupcBw/mqdefault.jpg"
+            },
+            {
+                order: 4,
+                name: "EMOM",
+                isSuperset: true,
+                isEMOM: true,
+                emomDetails: {
+                    totalIntervals: 8,
+                    workIntervalSeconds: 30
+                },
+                items: [
+                    {
+                        name: "Cuerpo Libre - Cardio - Jumping Jack",
+                        subOrder: 1,
+                        sets: 1,
+                        reps: "10-12",
+                        isEMOMItem: true,
+                        videoUrl: "https://youtu.be/K5PMB8CauGM",
+                        imageUrl: "https://i.ytimg.com/vi/K5PMB8CauGM/mqdefault.jpg"
+                    },
+                    {
+                        name: "Flexiones + Mountain Climbers",
+                        subOrder: 2,
+                        sets: 1,
+                        reps: "10",
+                        isEMOMItem: true,
+                        videoUrl: "https://www.youtube.com/shorts/1wdJ5RSUCBE",
+                        imageUrl: "https://i.ytimg.com/vi/1wdJ5RSUCBE/mqdefault.jpg"
+                    }
+                ],
+                notes: "",
+                videoUrl: "https://youtu.be/K5PMB8CauGM",
+                imageUrl: "https://i.ytimg.com/vi/K5PMB8CauGM/mqdefault.jpg"
+            },
+            {
+                order: 5,
+                name: "Circuito",
+                isSuperset: true,
+                circuitDetails: {
+                    totalRounds: 3,
+                    restBetweenExercisesSeconds: 15,
+                    restBetweenRoundsSeconds: 60
+                },
+                items: [
+                    {
+                        name: "Floor Press con Mancuernas",
+                        subOrder: 1,
+                        sets: 1,
+                        reps: "45s",
+                        isCircuitItem: true,
+                        videoUrl: "https://www.youtube.com/shorts/Iu02N3s7zgQ",
+                        imageUrl: "https://i.ytimg.com/vi/Iu02N3s7zgQ/mqdefault.jpg"
+                    },
+                    {
+                        name: "Hollow - Facial",
+                        subOrder: 2,
+                        sets: 1,
+                        reps: "45s",
+                        isCircuitItem: true,
+                        videoUrl: "https://youtu.be/5dnBA1cxYL0",
+                        imageUrl: "https://i.ytimg.com/vi/5dnBA1cxYL0/mqdefault.jpg"
+                    },
+                    {
+                        name: "Gomas - Banded hip thrust",
+                        subOrder: 3,
+                        sets: 1,
+                        reps: "45s",
+                        isCircuitItem: true,
+                        videoUrl: "https://www.youtube.com/watch?v=z8z0biVVaQY",
+                        imageUrl: "https://i.ytimg.com/vi/z8z0biVVaQY/mqdefault.jpg"
+                    }
+                ],
+                notes: "",
+                videoUrl: "https://www.youtube.com/shorts/Iu02N3s7zgQ",
+                imageUrl: "https://i.ytimg.com/vi/Iu02N3s7zgQ/mqdefault.jpg"
             }
         ]
     }
