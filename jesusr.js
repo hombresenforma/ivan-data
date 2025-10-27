080236bd888a1ac97c7f48663f71c66d6eb3d856
hombresenforma <ivan.hombresenforma@gmail.com>
2025-10-27 11:04:39 +0100

    Update jesusr.js

--- jesusr.js
+++ jesusr.js
@@ -1,3 +1,7 @@
+// Titulo: new_4d_fuerza_02
+// Notas: - Cambiamos a UNILATERAL y 6-8 reps.
+// - Añadimos 1 serie y DROP-SET en el segundo.
+
 // Contenido de app_datos.js
 
 // =================================================================================
@@ -13,19 +17,19 @@ const workoutData = {
                 sets: 4,
                 reps: "30, 10, 10, 10",
                 rest: "90s",
-                notes: "Primera serie BILBO (Máximo 30 reps, máximo 2 sesiones con misma carga). ",
+                notes: "",
                 videoUrl: "https://youtu.be/PKpsrFS2uac",
                 imageUrl: "https://i.ytimg.com/vi/PKpsrFS2uac/mqdefault.jpg"
             },
             {
                 order: 2,
-                name: "Press Militar Unilateral en Landmine",
+                name: "Press Militar Unilat de Pie con Mancuerna/KTB",
                 sets: 4,
-                reps: "4-6",
-                rest: "120s",
+                reps: "6-8",
+                rest: "90s",
                 notes: "",
-                videoUrl: "https://www.youtube.com/watch?v=p2OLvWOo9l0",
-                imageUrl: "https://i.ytimg.com/vi/p2OLvWOo9l0/mqdefault.jpg"
+                videoUrl: "https://www.youtube.com/shorts/vIK0qkXP_f0",
+                imageUrl: "https://i.ytimg.com/vi/vIK0qkXP_f0/mqdefault.jpg"
             },
             {
                 order: 3,
@@ -35,7 +39,7 @@ const workoutData = {
                     {
                         name: "Press Banca con Mancuernas",
                         subOrder: 1,
-                        sets: 3,
+                        sets: 4,
                         reps: "10-12",
                         isSupersetStart: true,
                         videoUrl: "https://youtu.be/hXCJC2Apcdg",
@@ -44,14 +48,14 @@ const workoutData = {
                     {
                         name: "Flexiones Tempo 1:1",
                         subOrder: 2,
-                        sets: 3,
+                        sets: 4,
                         reps: "10",
                         rest: "90s",
                         videoUrl: "https://www.youtube.com/shorts/-9Md7Htla-o",
                         imageUrl: "https://i.ytimg.com/vi/-9Md7Htla-o/mqdefault.jpg"
                     }
                 ],
-                notes: "",
+                notes: "Las 2 últimas series, con DROP-SET en el primer ejercicio.",
                 videoUrl: "https://youtu.be/hXCJC2Apcdg",
                 imageUrl: "https://i.ytimg.com/vi/hXCJC2Apcdg/mqdefault.jpg"
             },
@@ -90,21 +94,21 @@ const workoutData = {
         exercises: [
             {
                 order: 1,
-                name: "Peso Muerto Rumano con Mancuernas",
+                name: "Peso Muerto Rumano a una Pierna con Mancuerna",
                 sets: 4,
-                reps: "4-6",
-                rest: "120s",
+                reps: "6-8",
+                rest: "90s",
                 notes: "",
-                videoUrl: "https://www.youtube.com/shorts/SMll4DOYvEs",
-                imageUrl: "https://i.ytimg.com/vi/SMll4DOYvEs/mqdefault.jpg"
+                videoUrl: "https://www.youtube.com/watch?v=1bAi304kd7s",
+                imageUrl: "https://i.ytimg.com/vi/1bAi304kd7s/mqdefault.jpg"
             },
             {
                 order: 2,
                 name: "Dominadas con Peso Corporal",
-                sets: 3,
+                sets: 4,
                 reps: "8",
                 rest: "90s",
-                notes: "",
+                notes: "Las 2 últimas series, con DROP-SET (Puedes usar goma o hacer dominadas australianas en una barra baja)",
                 videoUrl: "https://www.youtube.com/shorts/pb-14sP_R4M",
                 imageUrl: "https://i.ytimg.com/vi/pb-14sP_R4M/mqdefault.jpg"
             },
@@ -181,13 +185,13 @@ const workoutData = {
             },
             {
                 order: 2,
-                name: "Hack Squat / Prensa Hack",
+                name: "Zancada Unilateral con Barra",
                 sets: 4,
-                reps: "4-6",
-                rest: "120s",
-                notes: "Trabajo de Fuerza. Lo más explosivo posible. Sin llegar al fallo.",
-                videoUrl: "https://www.youtube.com/watch?v=6teL-OyXuQs",
-                imageUrl: "https://i.ytimg.com/vi/6teL-OyXuQs/mqdefault.jpg"
+                reps: "6-8",
+                rest: "90s",
+                notes: "",
+                videoUrl: "https://youtu.be/6Mjkjmkhd5o",
+                imageUrl: "https://i.ytimg.com/vi/6Mjkjmkhd5o/mqdefault.jpg"
             },
             {
                 order: 3,
@@ -197,7 +201,7 @@ const workoutData = {
                     {
                         name: "Sentadilla Trasera en Multipower",
                         subOrder: 1,
-                        sets: 3,
+                        sets: 4,
                         reps: "10-12",
                         isSupersetStart: true,
                         videoUrl: "https://youtu.be/la-dqygoIuk",
@@ -206,14 +210,14 @@ const workoutData = {
                     {
                         name: "Sentadilla Goblet con Pies Elevados",
                         subOrder: 2,
-                        sets: 3,
+                        sets: 4,
                         reps: "12-15",
                         rest: "90s",
                         videoUrl: "https://youtu.be/E2n7V0o8B5A",
                         imageUrl: "https://i.ytimg.com/vi/E2n7V0o8B5A/mqdefault.jpg"
                     }
                 ],
-                notes: "",
+                notes: "Las 2 últimas series, con DROP-SET en el primer ejercicio.",
                 videoUrl: "https://youtu.be/la-dqygoIuk",
                 imageUrl: "https://i.ytimg.com/vi/la-dqygoIuk/mqdefault.jpg"
             },
@@ -244,21 +248,21 @@ const workoutData = {
         exercises: [
             {
                 order: 1,
-                name: "Remo con Barra",
+                name: "Remo Diagonal Unilat en Polea Alta",
                 sets: 4,
-                reps: "4-6",
-                rest: "120s",
-                notes: "Trabajo de Fuerza. Lo más explosivo posible. Sin llegar al fallo.",
-                videoUrl: "https://youtu.be/MjnZ52mZgT0",
-                imageUrl: "https://i.ytimg.com/vi/MjnZ52mZgT0/mqdefault.jpg"
+                reps: "6-8",
+                rest: "90s",
+                notes: "",
+                videoUrl: "https://youtube.com/shorts/ikKQhcynKmg",
+                imageUrl: "https://i.ytimg.com/vi/ikKQhcynKmg/mqdefault.jpg"
             },
             {
                 order: 2,
                 name: "Jalón al Pecho Neutro en Polea",
-                sets: 3,
+                sets: 4,
                 reps: "8",
                 rest: "90s",
-                notes: "",
+                notes: "Las 2 últimas series, con DROP-SET en el primer ejercicio.",
                 videoUrl: "https://youtu.be/5YzMH2KkMHc",
                 imageUrl: "https://i.ytimg.com/vi/5YzMH2KkMHc/mqdefault.jpg"
             },
@@ -315,16 +319,13 @@ const workoutData = {
 };
 
 const exerciseAlternatives = {
-    "Press Militar Unilateral en Landmine": [
-        { name: "Press Militar Unilat de Pie con Mancuerna/KTB", videoUrl: "https://www.youtube.com/shorts/vIK0qkXP_f0", imageUrl: "https://i.ytimg.com/vi/vIK0qkXP_f0/mqdefault.jpg" }
-    ],
     "Press Banca con Mancuernas": [
         { name: "Press Banca Inclinado en Multipower", videoUrl: "https://youtu.be/3GS7EjN7KSk", imageUrl: "https://i.ytimg.com/vi/3GS7EjN7KSk/mqdefault.jpg" }
     ],
     "Cruces en Polea Alta": [
         { name: "Aperturas en Banco Inclinado con Mancuernas", videoUrl: "https://youtu.be/Lx8d28YlcbQ", imageUrl: "https://i.ytimg.com/vi/Lx8d28YlcbQ/mqdefault.jpg" }
     ],
-    "Peso Muerto Rumano con Mancuernas": [
+    "Peso Muerto Rumano a una Pierna con Mancuerna": [
         { name: "", videoUrl: "", imageUrl: "" }
     ],
     "Remo Seal con Mancuernas": [
@@ -336,11 +337,14 @@ const exerciseAlternatives = {
     "Curl con Barra Z": [
         { name: "Curl en Polea Baja con Barra Recta", videoUrl: "https://www.youtube.com/watch?v=obFKEQEAink", imageUrl: "https://i.ytimg.com/vi/obFKEQEAink/mqdefault.jpg" }
     ],
-    "Hack Squat / Prensa Hack": [
+    "Zancada Unilateral con Barra": [
         { name: "Prensa Inclinada 45º", videoUrl: "https://www.youtube.com/watch?v=IqjbBRNqJps", imageUrl: "https://i.ytimg.com/vi/IqjbBRNqJps/mqdefault.jpg" }
     ],
     "Sentadilla Trasera en Multipower": [
         { name: "Sentadilla Trasera con Barra", videoUrl: "https://youtu.be/FK5XU_gaxAE", imageUrl: "https://i.ytimg.com/vi/FK5XU_gaxAE/mqdefault.jpg" }
+    ],
+    "Remo Diagonal Unilat en Polea Alta": [
+        { name: "Máquina de Remo (Gironda)", videoUrl: "https://www.youtube.com/watch?v=3wcaZqSfP0A", imageUrl: "https://i.ytimg.com/vi/3wcaZqSfP0A/mqdefault.jpg" }
     ],
     "Jalón al Pecho Neutro en Polea": [
         { name: "Dominadas con Peso Corporal", videoUrl: "https://www.youtube.com/shorts/pb-14sP_R4M", imageUrl: "https://i.ytimg.com/vi/pb-14sP_R4M/mqdefault.jpg" }
